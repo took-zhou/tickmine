5cdd8d1941c30cfbfbdaf6989c29c6a134cafccf
zhoufan <zhoufan@cdsslh.com>
2021-11-19 00:57:07 +0800

    [问题/需求描述]:更新依赖 [解决方法]:更新依赖 [影响范围]:无 [需求编号]:无
    Change-Id: I5fc40195817141e72bec75f348b2acb41eaaa25e

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(
 
     # 项目的依赖库，读取的requirements.txt内容
     install_requires = [
-        'numpy==1.19.5',
-        'pandas==1.1.5',
-        'rarfile==4.0',
-        'setuptools==39.0.1',
-        'streamlit==1.1.0',
-        'zerorpc==0.6.3'
+        'numpy>=1.19.5',
+        'pandas>=1.1.5',
+        'rarfile>=4.0',
+        'setuptools>=39.0.1',
+        'streamlit>=1.1.0',
+        'zerorpc>=0.6.3'
     ],
 
     # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
