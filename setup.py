750524bbc1ec0a44c5218d836f0816868999b672
zhoufan <zhoufan@cdsslh.com>
2022-08-16 13:57:08 +0000

    [问题/需求描述]:固定zerorpc和zmq版本 [解决方法]:固定zerorpc和zmq版本 [影响范围]:无 [需求编号]:无
    Change-Id: I90c32106aa6d2ec0f112c5cae8bfa2ea599c7613

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="tickmine",
-    version="3.6.2",
+    version="3.6.3",
     author="zhoufan",
     author_email="zhoufan@cdsslh.com",
     description="data layer",
@@ -11,7 +11,9 @@ setup(
     url="http://devpi.cdsslh.com:8090",
 
     # 项目的依赖库，读取的requirements.txt内容
-    install_requires=['numpy>=1.19.5', 'pandas>=1.1.5', 'rarfile>=4.0', 'setuptools>=39.0.1', 'zerorpc>=0.6.3', 'pytest>=7.0.1'],
+    install_requires=[
+        'numpy>=1.19.5', 'pandas>=1.1.5', 'rarfile>=4.0', 'setuptools>=39.0.1', 'zerorpc==0.6.3', 'pytest>=7.0.1', 'pyzmq==23.2.1'
+    ],
 
     # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
     packages=find_packages())
