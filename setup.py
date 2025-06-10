4ed8654784b8d28f32e47e054fb25762856e075e
zhoufan <zhoufan@tsaodai.com>
2025-06-10 16:59:17 +0800

    [问题/需求描述]:去除不必要的依赖 [解决方法]:去除不必要的依赖 [影响范围]:无 [需求编号]:无
    Change-Id: I54eaeb59bdd7c74018f5c6d941b78555efe42be5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url="https://devpi.tsaodai.com",
 
     # 项目的依赖库，读取的requirements.txt内容
-    install_requires=['pandas>=1.4.3', 'grpcio>=1.60.0', 'grpcio-tools>=1.60.0', 'pytest>=7.1.2', 'setuptools>=39.0.1'],
+    install_requires=['pandas>=1.4.3', 'grpcio>=1.60.0', 'pytest>=7.1.2', 'setuptools>=39.0.1'],
 
     # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
     packages=find_packages())
